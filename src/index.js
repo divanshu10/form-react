0fc040932e2f06f410970c4078ec3e9847fb39ba
divanshu sikarwar <divanshusikarwar123@gmail.com>
2022-12-04 18:26:54 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import CentralState from './Context/CentralState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    < HashRouter>
       <CentralState>
         <App />
       </CentralState>
-    </BrowserRouter>
+    </ HashRouter>
   </React.StrictMode>
 );
 
